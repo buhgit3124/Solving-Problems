8a3a893a6e7eb76e8211a1be0fe7a759e1e6302d
Shitanshu Shivam <shiva.shitanshu2018@gmail.com>
2022-07-14 22:39:18 +0530

    new

---
+++ dynamicArray.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+    int arr[size];
+    int *newArray = new int[size];
+
+    delete[]newArray;
+    newArray=NULL;
+
+    return 0;
+}
